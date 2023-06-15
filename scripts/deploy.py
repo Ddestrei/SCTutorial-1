f43deb7cf31b4e13aaead6337af188c2255f0f66
ddestrei <ddestrie@gmail.com>
2023-06-15 07:50:58 +0200

    Fifth commit

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -24,10 +24,7 @@ def deploy_simple_storage():
     print(simple_storage)
     return simple_storage
 
-proposals = [0,1,2]
-
 def deploy_ballot():
-    byte_proposal = [convert.to_bytes(p) for p in proposals]
     ballot = Ballot.deploy(
     ['A','B','C'],
     {
